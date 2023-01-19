3ae95ce62bdb6b34bf16286fd2b3cb9da37ca68d
skpratt <sarah.pratt@hashicorp.com>
2023-01-19 12:01:37 -0600

    updates all tokenID references to clarify accessorID

--- agent/consul/auth/token_writer_test.go
+++ agent/consul/auth/token_writer_test.go
@@ -494,10 +494,6 @@ func TestTokenWriter_Update_Validation(t *testing.T) {
 			token:         structs.ACLToken{AccessorID: token.AccessorID, ExpirationTime: timePointer(token.ExpirationTime.Add(1 * time.Minute))},
 			errorContains: "Cannot change expiration time",
 		},
-		"Type set": {
-			token:         structs.ACLToken{AccessorID: token.AccessorID, Type: "some-type"},
-			errorContains: "Type cannot be specified for this token",
-		},
 	}
 	for desc, tc := range testCases {
 		t.Run(desc, func(t *testing.T) {
